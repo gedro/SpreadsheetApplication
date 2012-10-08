d444875cca41f55fa2a062fba9320027361442bf
Gabor Harsanyi <harsanyigabor@gmail.com>
2012-10-09 01:01:46 +0300

    Changed gotocelldialog's buttons to  QDialogButtonBox

--- gotocelldialog.cpp
+++ gotocelldialog.cpp
@@ -5,13 +5,15 @@ GoToCellDialog::GoToCellDialog(QWidget *parent) : QDialog(parent) {
 
     setupUi(this);
 
+    buttonBox->button(QDialogButtonBox::Ok)->setEnabled(false);
+
     QRegExp regExp("[A-Za-z][1-9][0-9]{0,2}");
     lineEdit->setValidator(new QRegExpValidator(regExp, this));
 
-    connect(okButton, SIGNAL(clicked()), this, SLOT(accept()));
-    connect(cancelButton, SIGNAL(clicked()), this, SLOT(reject()));
+    connect(buttonBox, SIGNAL(accepted()), this, SLOT(accept()));
+    connect(buttonBox, SIGNAL(rejected()), this, SLOT(reject()));
 }
 
 void GoToCellDialog::on_lineEdit_textChanged() {
-    okButton->setEnabled(lineEdit->hasAcceptableInput());
+    buttonBox->button(QDialogButtonBox::Ok)->setEnabled( lineEdit->hasAcceptableInput() );
 }
