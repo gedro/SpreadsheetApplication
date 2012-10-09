9d3ed72bc652b04d7a04947ccb515f363962d24f
Gabor Harsanyi <harsanyigabor@gmail.com>
2012-10-09 03:48:19 +0300

    Creating Main Window (Part I.)

---
+++ mainwindow.cpp
@@ -0,0 +1,33 @@
+#include <QtGui>
+
+#include "finddialog.h"
+#include "gotocelldialog.h"
+#include "mainwindow.h"
+#include "sortdialog.h"
+#include "spreadsheet.h"
+
+MainWindow::MainWindow() {
+    spreadsheet = new Spreadsheet;
+    setCentralWidget(spreadsheet);
+
+    createActions();
+    createMenus();
+    createContextMenu();
+    createToolBars();
+    createStatusBar();
+
+    readSettings();
+
+    findDialog = 0;
+
+    setWindowIcon(QIcon(":/images/icon.png"));
+    setCurrentFile("");
+}
+
+void MainWindow::createActions() {
+    newAction = new QAction(tr("&New"), this);
+    newAction->setIcon(QIcon(":/images/new.png"));
+    newAction->setShortcut(QKeySequence::New);
+    newAction->setStatusTip(tr("Create a new spreadsheet file"));
+    connect(newAction, SIGNAL(triggered()), this, SLOT(newFile()));
+}
