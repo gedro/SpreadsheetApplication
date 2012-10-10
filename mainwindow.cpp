07702607d0ec5d6364d31dc86019d44ee263c314
Gabor Harsanyi <harsanyigabor@gmail.com>
2012-10-11 00:44:21 +0300

    Implemented private slots functions

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -175,3 +175,137 @@ void MainWindow::spreadsheetModified() {
     setWindowModified(true);
     updateStatusBar();
 }
+
+void MainWindow::newFile() {
+    if (okToContinue()) {
+        spreadsheet->clear();
+        setCurrentFile("");
+    }
+}
+
+bool MainWindow::okToContinue() {
+    if (isWindowModified()) {
+        int r = QMessageBox::warning(this, tr("Spreadsheet"),
+                        tr("The document has been modified.\nDo you want to save your changes?"),
+                        QMessageBox::Yes | QMessageBox::No | QMessageBox::Cancel);
+        if (r == QMessageBox::Yes) {
+            return save();
+        } else if (r == QMessageBox::Cancel) {
+            return false;
+        }
+    }
+    return true;
+}
+
+void MainWindow::open() {
+    if (okToContinue()) {
+        QString fileName = QFileDialog::getOpenFileName(this,
+                                   tr("Open Spreadsheet"), ".",
+                                   tr("Spreadsheet files (*.sp)"));
+        if (!fileName.isEmpty()) {
+            loadFile(fileName);
+        }
+    }
+}
+
+bool MainWindow::loadFile(const QString &fileName) {
+    if (!spreadsheet->readFile(fileName)) {
+        statusBar()->showMessage(tr("Loading canceled"), 2000);
+        return false;
+    }
+
+    setCurrentFile(fileName);
+    statusBar()->showMessage(tr("File loaded"), 2000);
+    return true;
+}
+
+bool MainWindow::save() {
+    if (curFile.isEmpty()) {
+        return saveAs();
+    } else {
+        return saveFile(curFile);
+    }
+}
+
+bool MainWindow::saveFile(const QString &fileName) {
+    if (!spreadsheet->writeFile(fileName)) {
+        statusBar()->showMessage(tr("Saving canceled"), 2000);
+        return false;
+    }
+
+    setCurrentFile(fileName);
+    statusBar()->showMessage(tr("File saved"), 2000);
+    return true;
+}
+
+bool MainWindow::saveAs() {
+    QString fileName = QFileDialog::getSaveFileName(this,
+                               tr("Save Spreadsheet"), ".",
+                               tr("Spreadsheet files (*.sp)"));
+
+    if (fileName.isEmpty()) {
+        return false;
+    }
+
+    return saveFile(fileName);
+}
+
+void MainWindow::closeEvent(QCloseEvent *event) {
+    if (okToContinue()) {
+        writeSettings();
+        event->accept();
+    } else {
+        event->ignore();
+    }
+}
+
+void MainWindow::setCurrentFile(const QString &fileName) {
+    curFile = fileName;
+    setWindowModified(false);
+    QString shownName = tr("Untitled");
+    if (!curFile.isEmpty()) {
+        shownName = strippedName(curFile);
+        recentFiles.removeAll(curFile);
+        recentFiles.prepend(curFile);
+        updateRecentFileActions();
+    }
+
+    setWindowTitle(tr("%1[*] - %2").arg(shownName)
+                                   .arg(tr("Spreadsheet")));
+}
+
+QString MainWindow::strippedName(const QString &fullFileName) {
+    return QFileInfo(fullFileName).fileName();
+}
+
+void MainWindow::updateRecentFileActions() {
+    QMutableStringListIterator i(recentFiles);
+    while (i.hasNext()) {
+        if (!QFile::exists(i.next())) {
+            i.remove();
+        }
+    }
+
+    for (int j = 0; j < MaxRecentFiles; ++j) {
+        if (j < recentFiles.count()) {
+            QString text = tr("&%1 %2")
+                           .arg(j + 1)
+                           .arg(strippedName(recentFiles[j]));
+            recentFileActions[j]->setText(text);
+            recentFileActions[j]->setData(recentFiles[j]);
+            recentFileActions[j]->setVisible(true);
+        } else {
+            recentFileActions[j]->setVisible(false);
+        }
+    }
+    separatorAction->setVisible(!recentFiles.isEmpty());
+}
+
+void MainWindow::openRecentFile() {
+    if (okToContinue()) {
+        QAction *action = qobject_cast<QAction *>(sender());
+        if (action) {
+            loadFile(action->data().toString());
+        }
+    }
+}
