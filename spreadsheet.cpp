ede1faae20c5e33606b45130896a514bdd36d0cf
Gabor Harsanyi <harsanyigabor@gmail.com>
2012-10-14 01:21:59 +0300

    Implemented the functionalities of the edit mennu

--- spreadsheet.cpp
+++ spreadsheet.cpp
@@ -140,3 +140,123 @@ bool Spreadsheet::readFile(const QString &fileName) {
 
     return true;
 }
+
+void Spreadsheet::cut() {
+    copy();
+    del();
+}
+
+void Spreadsheet::copy() {
+    QTableWidgetSelectionRange range = selectedRange();
+    QString str;
+
+    for (int i = 0; i < range.rowCount(); ++i) {
+        if (i > 0) {
+            str += "\n";
+        }
+        for (int j = 0; j < range.columnCount(); ++j) {
+            if (j > 0) {
+                str += "\t";
+            }
+            str += formula(range.topRow() + i, range.leftColumn() + j);
+        }
+    }
+    QApplication::clipboard()->setText(str);
+}
+
+QTableWidgetSelectionRange Spreadsheet::selectedRange() const {
+    QList<QTableWidgetSelectionRange> ranges = selectedRanges();
+    if (ranges.isEmpty()) {
+        return QTableWidgetSelectionRange();
+    }
+
+    return ranges.first();
+}
+
+void Spreadsheet::paste() {
+    QTableWidgetSelectionRange range = selectedRange();
+    QString str = QApplication::clipboard()->text();
+    QStringList rows = str.split('\n');
+    int numRows = rows.count();
+    int numColumns = rows.first().count('\t') + 1;
+
+    if (range.rowCount() * range.columnCount() != 1
+            && (range.rowCount() != numRows
+                || range.columnCount() != numColumns)) {
+
+        QMessageBox::information(this, tr("Spreadsheet"),
+                tr("The information cannot be pasted because the copy "
+                   "and paste areas aren't the same size."));
+        return;
+    }
+
+    for (int i = 0; i < numRows; ++i) {
+        QStringList columns = rows[i].split('\t');
+        for (int j = 0; j < numColumns; ++j) {
+            int row = range.topRow() + i;
+            int column = range.leftColumn() + j;
+            if (row < RowCount && column < ColumnCount)
+                setFormula(row, column, columns[j]);
+        }
+    }
+
+    somethingChanged();
+}
+
+void Spreadsheet::del() {
+    QList<QTableWidgetItem *> items = selectedItems();
+    if (!items.isEmpty()) {
+        foreach (QTableWidgetItem *item, items) {
+            delete item;
+        }
+        somethingChanged();
+    }
+}
+
+void Spreadsheet::selectCurrentRow() {
+    selectRow(currentRow());
+}
+
+void Spreadsheet::selectCurrentColumn() {
+    selectColumn(currentColumn());
+}
+
+void Spreadsheet::findNext(const QString &str, Qt::CaseSensitivity cs) {
+    int row = currentRow();
+    int column = currentColumn() + 1;
+
+    while (row < RowCount) {
+        while (column < ColumnCount) {
+            if (text(row, column).contains(str, cs)) {
+                clearSelection();
+                setCurrentCell(row, column);
+                activateWindow();
+                return;
+            }
+            ++column;
+        }
+        column = 0;
+        ++row;
+    }
+    QApplication::beep();
+}
+
+void Spreadsheet::findPrevious(const QString &str, Qt::CaseSensitivity cs) {
+    int row = currentRow();
+    int column = currentColumn() - 1;
+
+    while (row >= 0) {
+        while (column >= 0) {
+            if (text(row, column).contains(str, cs)) {
+                clearSelection();
+                setCurrentCell(row, column);
+                activateWindow();
+                return;
+            }
+            --column;
+        }
+        column = ColumnCount - 1;
+        --row;
+    }
+    QApplication::beep();
+}
